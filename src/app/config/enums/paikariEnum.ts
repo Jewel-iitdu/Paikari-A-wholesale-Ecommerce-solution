3e0d1173f42d3a9750cbf536c280093c503897c0
Jewel <bsse0608@iit.du.ac.bd>
2019-09-13 12:38:29 +0600

    initial modules

---
+++ src/app/config/enums/paikariEnum.ts
@@ -0,0 +1,5 @@
+export enum Roles {
+    Customer = 'Customer',
+    Supplier = 'Supplier',
+    Admin = 'Admin'
+}
---
+++ src/app/root/app.module.ts
@@ -0,0 +1,33 @@
+import { SharedModule } from './../shared/shared.module';
+import { ConfigModule } from './../config/config.module';
+import { CoreModule } from './../core/core.module';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './root-default/app.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { NavSideBarComponent } from './nav-side-bar/nav-side-bar.component';
+import { BlankComponent } from './blank/blank.component';
+import { RootService } from './services/root.service';
+
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NotFoundComponent,
+    NavSideBarComponent,
+    BlankComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    SharedModule,
+    CoreModule,
+    ConfigModule
+  ],
+  providers: [RootService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
