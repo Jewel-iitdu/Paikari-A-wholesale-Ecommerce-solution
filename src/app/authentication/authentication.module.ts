a46a0ff0b64dfecc0d1a89ceafa34d269f1e1659
Jewel <bsse0608@iit.du.ac.bd>
2019-09-13 15:48:27 +0600

    auth structure

--- src/app/authentication/authentication.module.ts
+++ src/app/authentication/authentication.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ComponentsComponent } from './components/components.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [ComponentsComponent],
   imports: [
     CommonModule
   ]
