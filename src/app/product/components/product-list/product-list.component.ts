a0ce52b7e574b8db07de443bf508f73b4371a003
Jewel <bsse0608@iit.du.ac.bd>
2019-11-04 13:39:35 +0600

    mat table problem

--- src/app/product/components/product-list/product-list.component.ts
+++ src/app/product/components/product-list/product-list.component.ts
@@ -2,23 +2,23 @@ import { Component, OnInit, ViewChild } from "@angular/core";
 import { MatTableDataSource, MatSort, MatPaginator } from '@angular/material';
 
 export interface PeriodicElement {
-  name: string;
-  position: number;
-  weight: number;
-  symbol: string;
+  date: string;
+  transactionId: number;
+  amount: number;
+  status: string;
 }
 
 const ELEMENT_DATA: PeriodicElement[] = [
-  {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
-  {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He'},
-  {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li'},
-  {position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be'},
-  {position: 5, name: 'Boron', weight: 10.811, symbol: 'B'},
-  {position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C'},
-  {position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N'},
-  {position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O'},
-  {position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F'},
-  {position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne'},
+  { transactionId: 1, date: "Hydrogen", amount: 1.0079, status: "H" },
+  { transactionId: 2, date: "Helium", amount: 4.0026, status: "He" },
+  { transactionId: 3, date: "Lithium", amount: 6.941, status: "Li" },
+  { transactionId: 4, date: "Beryllium", amount: 9.0122, status: "Be" },
+  { transactionId: 5, date: "Boron", amount: 10.811, status: "B" },
+  { transactionId: 6, date: "Carbon", amount: 12.0107, status: "C" },
+  { transactionId: 7, date: "Nitrogen", amount: 14.0067, status: "N" },
+  { transactionId: 8, date: "Oxygen", amount: 15.9994, status: "O" },
+  { transactionId: 9, date: "Fluorine", amount: 18.9984, status: "F" },
+  { transactionId: 10, date: "Neon", amount: 20.1797, status: "Ne" }
 ];
 
 @Component({
@@ -28,7 +28,7 @@ const ELEMENT_DATA: PeriodicElement[] = [
 })
 export class ProductListComponent implements OnInit {
 
-  displayedColumns: string[] = ['position', 'name', 'weight', 'symbol'];
+  displayedColumns: string[] = ["transactionId", "date", "amount", "status"];
   dataSource = new MatTableDataSource(ELEMENT_DATA);
 
   @ViewChild(MatSort, { static: true }) sort: MatSort;
