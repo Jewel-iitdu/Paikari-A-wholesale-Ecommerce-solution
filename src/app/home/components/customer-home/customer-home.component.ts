ca46593fe61b6e16a28245b6d6152c17cf44d427
Jewel <bsse0608@iit.du.ac.bd>
2019-12-15 01:19:03 +0600

    Design home page done

--- src/app/home/components/customer-home/customer-home.component.ts
+++ src/app/home/components/customer-home/customer-home.component.ts
@@ -1,60 +1,91 @@
+import { SharedService } from 'src/app/shared/services/shared.service';
+import { UtilityService } from 'src/app/core/utility-service/utility.service';
 import { ProductService } from './../../../product/services/product.service';
 import { OrderInformation } from './../../../config/interfaces/order.interface';
 import { OrderService } from './../../../order/services/order.service';
-import { ProductInformation } from "./../../../config/interfaces/product.interface";
-import { CustomerHomeService } from "./../../services/customer-home.service";
-import { Component, OnInit } from "@angular/core";
+import { ProductInformation } from './../../../config/interfaces/product.interface';
+import { CustomerHomeService } from './../../services/customer-home.service';
+import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
-import * as firebase from "firebase/app";
+import * as firebase from 'firebase/app';
 
 @Component({
-  selector: "app-customer-home",
-  templateUrl: "./customer-home.component.html",
-  styleUrls: ["./customer-home.component.scss"]
+	selector: 'app-customer-home',
+	templateUrl: './customer-home.component.html',
+	styleUrls: [ './customer-home.component.scss' ]
 })
 export class CustomerHomeComponent implements OnInit {
-  productData: ProductInformation;
-  firestoreData: Observable<any[]>;
-  orderInfo: OrderInformation;
-  singleProduct: ProductInformation;
-  userID: string;
-  constructor(
-    private homeService: CustomerHomeService,
-    private orderService: OrderService, private productService: ProductService
-  ) {}
+	productData: ProductInformation[] = [];
+	filterData: ProductInformation[] = [];
 
-  ngOnInit() {
-    
-    this.orderService.getUserId().subscribe(res=>{
-      this.userID = res;
-    })
+	firestoreData: Observable<any[]>;
+	orderInfo: OrderInformation;
+	singleProduct: ProductInformation;
+	userID: string;
+	constructor(
+		private homeService: CustomerHomeService,
+		private orderService: OrderService,
+		private productService: ProductService,
+		private utility: UtilityService,
+		private sharedService: SharedService
+	) {}
 
-    this.homeService.getAllProducts().subscribe(
-      list => {
-          const products = list.map(item => {
-          return {
-            id: item.payload.doc.id,
-            ...item.payload.doc.data()
-          }
-        });
-        
-        this.productData = products;
-        // console.log(this.productData)
-  })
-    
-  }
+	ngOnInit() {
+		this.setUserID();
+		this.setAllProducts();
+	}
 
-  addToCart(product){
-    this.singleProduct =product;
-    // console.log(this.singleProduct)
-    this.orderInfo ={
-      productID: this.singleProduct.id,
-      orderQuantity: this.singleProduct.productquantity,
-      payment: false,
-      date: firebase.firestore.FieldValue.serverTimestamp(),
-      userID: this.userID
-    }
-    this.orderService.createOrAddCart(this.orderInfo);
-  }
+	setUserID() {
+		this.orderService.getUserId().subscribe((res) => {
+			this.userID = res;
+		});
+	}
 
+	setAllProducts() {
+		this.homeService.getAllProducts().subscribe((list) => {
+			const products = list.map((item) => {
+				return {
+					id: item.payload.doc.id,
+					...item.payload.doc.data()
+				};
+			});
+			this.productData = products;
+			this.filterData = products;
+		});
+	}
+
+	addToCart(product) {
+    this.sharedService.showSpinner();
+
+		this.singleProduct = product;
+		this.orderInfo = {
+			productID: this.singleProduct.id,
+			orderQuantity: this.singleProduct.productquantity,
+			payment: false,
+			date: firebase.firestore.FieldValue.serverTimestamp(),
+			userID: this.userID
+		};
+    this.orderService.createOrAddCart(this.orderInfo).subscribe(res=>{
+      this.sharedService.hideSpinner();
+
+    });
+		this.addedToCartSnackbar();
+	}
+
+	addedToCartSnackbar() {
+		this.sharedService.openSnackBar({
+      duration:3,
+      data: {
+				isAccepted: true,
+				message: 'Product Added to cart'
+      },
+      panelClass: [ 'recovery-snackbar' ]
+		});
+	}
+
+	applyFilter(value) {
+		value = String(value);
+		let x = this.utility.checkIfExists(value, this.filterData);
+		this.productData = x;
+	}
 }
--- src/app/shared/services/shared.service.ts
+++ src/app/shared/services/shared.service.ts
@@ -3,29 +3,33 @@ import { MatSnackBar, MatDialog } from '@angular/material';
 import { SnackbarComponent } from '../components/snackbar/snackbar.component';
 import { snackbar } from '../../config/interfaces/config.interface';
 import { BehaviorSubject } from 'rxjs';
+import { NgxSpinnerService } from 'ngx-spinner';
 // import { PasswordChangeComponent } from 'src/app/profile/components/password-change/password-change.component';
 
 @Injectable({
-  providedIn: 'root'
+	providedIn: 'root'
 })
 export class SharedService {
+	// Username = new BehaviorSubject('');
+	// $username= this.Username.asObservable();
+	// menuIndex = new BehaviorSubject<number>(1);
+	// $menuIndex = this.menuIndex.asObservable();
+	constructor(private snackbar: MatSnackBar, public dialog: MatDialog, private spinner: NgxSpinnerService) {}
 
-  // Username = new BehaviorSubject('');
-  // $username= this.Username.asObservable();
-  // menuIndex = new BehaviorSubject<number>(1);
-  // $menuIndex = this.menuIndex.asObservable();
-  constructor(private snackbar: MatSnackBar,public dialog: MatDialog) { }
-
-  openSnackBar(configuration:snackbar) {
+	openSnackBar(configuration: snackbar) {
 		this.snackbar.openFromComponent(SnackbarComponent, {
-		  duration: (configuration.duration?configuration.duration:1) * 1000,
-      data: configuration.data,
-      horizontalPosition:(configuration.horizontalPosition?configuration.horizontalPosition:'right'),
-      verticalPosition:(configuration.verticalPosition?configuration.verticalPosition:'bottom'),
-      panelClass:configuration.panelClass?configuration.panelClass:null ,
-      
+			duration: (configuration.duration ? configuration.duration : 1) * 1000,
+			data: configuration.data,
+			horizontalPosition: configuration.horizontalPosition ? configuration.horizontalPosition : 'right',
+			verticalPosition: configuration.verticalPosition ? configuration.verticalPosition : 'top',
+			panelClass: configuration.panelClass ? configuration.panelClass : null
 		});
-    }
-    
-    
+  }
+  
+  showSpinner(){
+    this.spinner.show();
+  }
+  hideSpinner(){
+    this.spinner.hide();
+  }
 }
